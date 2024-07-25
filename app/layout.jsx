3d7e56d268817d1127452423177e8cafded86554
Edward Kim <itgma1231@gmail.com>
2024-07-25 17:34:05 -0400

    layout.jsx - tailwindcss directives imported; metadata object configured

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,11 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+    title: 'PropertyListing | Find A Rental Property',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+}
+
 const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
