d7db25a6fb3cfc6d5a6ad9f4170821ac2619a2a9
Edward Kim <itgma1231@gmail.com>
2024-07-28 21:59:34 -0400

    Navbar.jsx - set initial state; onClick() toggle profile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,14 @@
 'use client';
-
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@/assets/images/logo-white.png';
 import defaultProfile from '@/assets/images/profile.png';
-
 import { FaGoogle } from 'react-icons/fa6';
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const [profileMenu, setProfileMenu] = useState(false);
 
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
@@ -119,51 +118,54 @@ const Navbar = () => {
                   id='user-menu-button'
                   aria-expanded='false'
                   aria-haspopup='true'
+                  onClick={() => setProfileMenu((prev) => !prev)}
                 >
                   <span className='absolute -inset-1.5'></span>
                   <span className='sr-only'>Open user menu</span>
                   <Image
                     className='h-8 w-8 rounded-full'
                     src={defaultProfile}
-                    alt=''
+                    alt='default-profile-icon'
                   />
                 </button>
               </div>
-              <div
-                id='user-menu'
-                className='hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-                role='menu'
-                aria-orientation='vertical'
-                aria-labelledby='user-menu-button'
-                tabIndex='-1'
-              >
-                <Link
-                  href='/profile'
-                  className='block px-4 py-2 text-sm text-gray-700'
-                  role='menuitem'
+              {profileMenu && (
+                <div
+                  id='user-menu'
+                  className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                  role='menu'
+                  aria-orientation='vertical'
+                  aria-labelledby='user-menu-button'
                   tabIndex='-1'
-                  id='user-menu-item-0'
                 >
-                  Your Profile
-                </Link>
-                <Link
-                  href='/properties/saved'
-                  className='block px-4 py-2 text-sm text-gray-700'
-                  role='menuitem'
-                  tabIndex='-1'
-                  id='user-menu-item-2'
-                >
-                  Saved Properties
-                </Link>
-                <button
-                  className='block px-4 py-2 text-sm text-gray-700'
-                  role='menuitem'
-                  tabIndex='-1'
-                  id='user-menu-item-2'
-                >
-                  Sign Out
-                </button>
-              </div>
+                  <Link
+                    href='/profile'
+                    className='block px-4 py-2 text-sm text-gray-700'
+                    role='menuitem'
+                    tabIndex='-1'
+                    id='user-menu-item-0'
+                  >
+                    Your Profile
+                  </Link>
+                  <Link
+                    href='/properties/saved'
+                    className='block px-4 py-2 text-sm text-gray-700'
+                    role='menuitem'
+                    tabIndex='-1'
+                    id='user-menu-item-2'
+                  >
+                    Saved Properties
+                  </Link>
+                  <button
+                    className='block px-4 py-2 text-sm text-gray-700'
+                    role='menuitem'
+                    tabIndex='-1'
+                    id='user-menu-item-2'
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         </div>
