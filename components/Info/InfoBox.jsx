d7621e390d4a2c4de5d6e32a6b818b1e2e3f47e7
Edward Kim <itgma1231@gmail.com>
2024-07-31 18:45:09 -0400

    anchor tag to <Link /> quick fix

--- components/Info/InfoBox.jsx
+++ components/Info/InfoBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const InfoBox = ({ children, heading, btnInfo, bgColor = 'bg-blue-100' }) => {
   return (
@@ -7,12 +8,12 @@ const InfoBox = ({ children, heading, btnInfo, bgColor = 'bg-blue-100' }) => {
       <p className='mt-2 mb-4'>
         {children}
       </p>
-      <a
+      <Link
         href={btnInfo.link}
         className='inline-block bg-blue-500 text-white w-44 mt-2 rounded-lg px-4 py-2 hover:bg-blue-400'
       >
         {btnInfo.text}
-      </a>
+      </Link>
     </div>
   );
 };
