27bdcd3963d226c53505850633561f47155a8809
Edward Kim <itgma1231@gmail.com>
2024-08-03 19:55:27 -0400

    HomeProperties.jsx - render most recent properties card

--- components/Home/HomeProperties.jsx
+++ components/Home/HomeProperties.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import PropertyCard from '../PropertyCard';
+import properties from '@/properties/properties.json';
 
 const HomeProperties = () => {
-  return (
-    <div>HomeProperties</div>
-  )
-}
+  const mostRecent = properties.slice(0, 3);
 
-export default HomeProperties
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        <h2 className='text-3xl font-bold text-center text-blue-500 mb-6 md:text-left'>
+          Most Recent
+        </h2>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {mostRecent.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default HomeProperties;
