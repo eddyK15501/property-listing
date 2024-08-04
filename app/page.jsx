3712911ee2766d50fe025f908564dd6d221cb527
Edward Kim <itgma1231@gmail.com>
2024-08-04 17:27:36 -0400

    .env MONGODB_URI var set; MongoDB Atlas cluster deployed

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,10 @@ import InfoBoxes from '@/components/Home/Info/InfoBoxes';
 import HomeProperties from '@/components/Home/HomeProperties';
 
 const Homepage = () => {
+  console.log(process.env.MONGODB_URI)
+  console.log(process.env.PUBLIC_DOMAIN)
+  console.log(process.env.PUBLIC_API_DOMAIN)
+
   return (
     <div>
       <Hero />
