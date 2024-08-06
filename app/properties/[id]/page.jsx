95bcce986a16bd7a1e8a6a49b6db531dbf43dc37
Edward Kim <itgma1231@gmail.com>
2024-08-06 14:08:48 -0400

    app/properties/[id]/page.jsx - <Link> route back to /properties; icon added

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import connectDB from '@/config/connection';
 import Property from '@/models/Property';
 import PropertyBanner from '@/components/Property/PropertyBanner';
+import { FaArrowCircleLeft } from 'react-icons/fa';
 
 const PropertyDetailsPage = async ({ params }) => {
   await connectDB();
@@ -9,6 +11,16 @@ const PropertyDetailsPage = async ({ params }) => {
   return (
     <>
       <PropertyBanner src={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='flex items-center font-medium text-blue-500 hover:text-blue-400'
+          >
+            <FaArrowCircleLeft size='22' className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
       <section>
         {property.name} {params.id}
       </section>
