c7e473d1565abd2213dd41be518d11bd1207ad87
Edward Kim <itgma1231@gmail.com>
2024-08-17 19:32:29 -0400

    set/update initial viewState; render if loading/error true

--- components/Property/PropertyMap.jsx
+++ components/Property/PropertyMap.jsx
@@ -7,6 +7,11 @@ const PropertyMap = ({ property }) => {
   const [error, setError] = useState(false);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [viewState, setViewState] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 10,
+  });
 
   setDefaults({
     key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
@@ -26,9 +31,10 @@ const PropertyMap = ({ property }) => {
           return;
         }
 
-        console.log(response);
         const { lat, lng } = response.results[0].geometry.location;
-        console.log(lat, lng);
+        setLatitude(lat);
+        setLongitude(lng);
+        setViewState({ ...viewState, latitude: lat, longitude: lng });
       } catch (err) {
         console.log(err);
         setError(true);
@@ -40,7 +46,11 @@ const PropertyMap = ({ property }) => {
     fetchCoordinate();
   }, []);
 
-  return <div>PropertyMap</div>;
+  if (loading) return <h3>Loading...</h3>;
+
+  if (error) return <div className='text-xl'>Location Not Found</div>;
+
+  return <div>Latitude: {latitude}, Longitude: {longitude}</div>;
 };
 
 export default PropertyMap;
