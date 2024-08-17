11298c0cf7add08512405edf025c47aaf825bd6b
Edward Kim <itgma1231@gmail.com>
2024-08-17 18:47:03 -0400

    react-geocode setDefaults() with API_KEY; async/await response

--- components/Property/PropertyMap.jsx
+++ components/Property/PropertyMap.jsx
@@ -1,9 +1,39 @@
 'use client';
+import { useState, useEffect } from 'react';
+import { setDefaults, fromAddress } from 'react-geocode';
 
 const PropertyMap = ({ property }) => {
-  return (
-    <div>PropertyMap</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
-export default PropertyMap
+  setDefaults({
+    key: process.env.GOOGLE_GEOCODING_API_KEY,
+    language: 'en',
+    region: 'us',
+  });
+
+  useEffect(() => {
+    const fetchCoordinate = async () => {
+      try {
+        const response = await fromAddress(
+          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+        );
+        
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoordinate();
+  }, []);
+
+  return <div>PropertyMap</div>;
+};
+
+export default PropertyMap;
