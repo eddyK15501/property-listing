3e4cf99bab88b0946468cda66845677bfd5de79d
Edward Kim <itgma1231@gmail.com>
2024-08-19 17:26:28 -0400

    PropertyContactForm.jsx component build; import react-icons/ri

--- components/Aside/BookmarkButton.jsx
+++ components/Aside/BookmarkButton.jsx
@@ -2,8 +2,8 @@ import { FaRegBookmark } from 'react-icons/fa';
 
 const BookmarkButton = ({ property }) => {
   return (
-    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full mr-4 py-2 px-4 rounded-full flex items-center justify-center'>
-      <FaRegBookmark className='mr-2.5' /> Bookmark
+    <button className='bg-blue-500 hover:bg-blue-600 text-white font-medium w-full mr-4 py-2 px-4 rounded-full flex items-center justify-center'>
+      <FaRegBookmark className='mr-2' /> Bookmark
     </button>
   );
 };
--- components/Aside/ShareButton.jsx
+++ components/Aside/ShareButton.jsx
@@ -2,8 +2,8 @@ import { LuShare2 } from 'react-icons/lu';
 
 const ShareButton = ({ property }) => {
   return (
-    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-      <LuShare2 className='mr-2.5 align-middle' /> Share Property
+    <button className='bg-rose-500 hover:bg-rose-600 text-white font-medium w-full py-2 px-4 rounded-full flex items-center justify-center'>
+      <LuShare2 className='mr-2' /> Share Property
     </button>
   );
 };
