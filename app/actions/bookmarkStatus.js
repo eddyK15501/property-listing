ef3bcd6ba38092921efb9e91a8586a1a4d2284b3
Edward Kim <itgma1231@gmail.com>
2024-08-20 12:32:47 -0400

    new action created to check user bookmark status

---
+++ app/actions/bookmarkStatus.js
@@ -0,0 +1,28 @@
+'use server';
+import connectDB from '@/config/connection';
+import User from '@/models/User';
+import { sessionUser } from '@/utils/sessionUser';
+
+export async function bookmarkStatus(propertyId) {
+  try {
+    await connectDB();
+    const getSessionUser = await sessionUser();
+
+    if (!getSessionUser || !getSessionUser.userId) {
+      throw new Error('User ID not found');
+    }
+
+    const { userId } = getSessionUser;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    let isBookmarked = user.bookmarks.includes(propId);
+
+    return { isBookmarked };
+  } catch (err) {
+    return { error: err.message };
+  }
+}
