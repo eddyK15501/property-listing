9d46957062394aeb755dd5eb6151768834dcd8f9
Edward Kim <itgma1231@gmail.com>
2024-08-20 21:14:27 -0400

    npm install react-share; configure url/share component properties

--- components/Aside/ShareButton.jsx
+++ components/Aside/ShareButton.jsx
@@ -1,10 +1,50 @@
-import { LuShare2 } from 'react-icons/lu';
+'use client';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  RedditShareButton,
+  LinkedinShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  RedditIcon,
+  LinkedinIcon,
+} from 'react-share';
 
 const ShareButton = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
   return (
-    <button className='bg-rose-500 hover:bg-rose-600 text-white font-medium w-full py-2 px-4 rounded-xl flex items-center justify-center'>
-      <LuShare2 className='mr-2' /> Share Property
-    </button>
+    <div className='bg-slate-50 rounded-lg shadow-md'>
+      <h3 className='text-md font-bold text-center pt-2'>
+        Share This Property:
+      </h3>
+      <div className='flex justify-center gap-3 pb-4 !mt-2'>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type.replace(/\s/g, '')}ForRent`}
+        >
+          <FacebookIcon size={35} borderRadius={10} />
+        </FacebookShareButton>
+        <RedditShareButton url={shareUrl} title={property.name}>
+          <RedditIcon size={35} borderRadius={10} bgStyle={{ fill: 'red' }} />
+        </RedditShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`${property.type.replace(/\s/g, '')}ForRent`]}
+        >
+          <TwitterIcon size={35} borderRadius={10} />
+        </TwitterShareButton>
+        <LinkedinShareButton
+          url={shareUrl}
+          title={property.name}
+          summary={property.description}
+        >
+          <LinkedinIcon size={35} borderRadius={10} />
+        </LinkedinShareButton>
+      </div>
+    </div>
   );
 };
 
